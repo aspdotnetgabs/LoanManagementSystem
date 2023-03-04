64253d6e291607d8625492a60371489336416a4c
Bernard Gabon <hewbertgabon@gmail.com>
2023-03-05 05:40:09 +0800

    rearrange amrt and pmt

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -22,8 +22,8 @@ namespace EdwillLoanAppMVC4.Models
 
         public DateTime StartDate { get; set; } // Amortization start date
 
-        public virtual List<Payment> Payments { get; set; }
-
         public virtual List<Amortization> Amortizations { get; set; }
+
+        public virtual List<Payment> Payments { get; set; }
     }
 }
